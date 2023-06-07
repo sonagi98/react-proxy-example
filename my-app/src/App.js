f586787c312977edb3f5998f5e8e12588fb877e5
sonagi98 <ubini01@gmail.com>
2023-06-07 14:31:54 +0900

    과제 제출

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,13 +5,17 @@ import BookTable from './components/BookTable';
 import DisplayBoard from './components/DisplayBoard';
 import CreateBook from './components/CreateBook';
 import { getAllBooks, createBook } from './services/BookService';
+import { getAllTodos, createTodo } from './services/TodoService';
 import Footer from './components/Footer';
+import DisplayBoard_Todo from './components/DisplayBoard_Todo';
 
 function App () {
 
   const [bookShelf, setBookShelf] = useState({});
   const [books, setBooks] = useState([]);
   const [numberOfBooks, setNumberBooks] = useState(0);
+  const [todos, setTodos] = useState([]);
+  const [numberOfTodos, setNumberTodos] = useState(0);
 
   const handleSubmit = () => {
       createBook(bookShelf)
@@ -26,6 +30,14 @@ function App () {
         setBooks(data);
         setNumberBooks(data.length);
       });
+  }
+
+  const getAllTodo = () => {
+    getAllTodos()
+      .then(data => {
+        setTodos(data);
+        setNumberTodos(data.length);
+      })
   }
 
   const handleOnChangeForm = (e) => {
@@ -55,6 +67,10 @@ function App () {
           getAllBook={getAllBook} 
         />
         <BookTable books={books} />
+        <DisplayBoard_Todo
+          numberOfTodos={numberOfTodos}
+          getAllTodo={getAllTodo}
+        />
         <Footer />
       </div>
     </div>
---
+++ my-app/src/components/DisplayBoard_Todo.js
@@ -0,0 +1,20 @@
+const DisplayBoard_Todo = ({numberOfTodos, getAllTodo}) => {
+    
+    return(
+        <div className="display-wrapper">
+            <div className="display-box">
+                <div className="display-board">
+                    <h4>생성된 수</h4>
+                    <div className="number">
+                    {numberOfTodos}
+                    </div>
+                </div>
+                <div className="get-button">
+                    <button onClick={() => getAllTodo()}>Get all Todos</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DisplayBoard_Todo;
